4ad6e242365f22164dab0682c280ba9cc8297711
David Novák <kaladivo@gmail.com>
2023-01-26 15:43:57 +0100

    Add endpoint to test logging

--- contact/src/main/java/com/cleevio/vexl/module/user/controller/InternalController.java
+++ contact/src/main/java/com/cleevio/vexl/module/user/controller/InternalController.java
@@ -3,19 +3,31 @@ package com.cleevio.vexl.module.user.controller;
 import com.cleevio.vexl.module.user.service.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User")
 @RestController
 @RequestMapping(value = "/internal")
 @RequiredArgsConstructor
+@Slf4j
 public class InternalController {
-        private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-        @PostMapping("/fail")
-        void refreshUser() {
-            this.userRepository.shouldFail();
+    @PostMapping("/fail")
+    void refreshUser() {
+        this.userRepository.shouldFail();
+    }
+
+    @PostMapping("/test-log")
+    @ResponseStatus(HttpStatus.OK)
+    void testLog(@RequestBody TestRequest request) {
+        switch (request.logLevel()) {
+            case "INFO" -> log.info(request.textToLog());
+            case "WARN" -> log.warn(request.textToLog());
+            case "ERROR" -> log.error(request.textToLog());
+            default -> log.debug(request.textToLog());
         }
+    }
 }
---
+++ contact/src/main/java/com/cleevio/vexl/module/user/controller/TestRequest.java
@@ -0,0 +1,7 @@
+package com.cleevio.vexl.module.user.controller;
+
+public record TestRequest(
+        String textToLog,
+        String logLevel
+) {
+}
