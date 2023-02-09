d03f239eb32e60d801e5208a949c05b32cbac695
David Novák <kaladivo@gmail.com>
2023-02-09 12:28:54 +0100

    Modify TTL notification copy

--- contact/src/main/java/com/cleevio/vexl/module/user/dto/InactivityNotificationDto.java
+++ contact/src/main/java/com/cleevio/vexl/module/user/dto/InactivityNotificationDto.java
@@ -5,11 +5,11 @@ import com.cleevio.vexl.module.user.constant.Platform;
 public interface InactivityNotificationDto {
 
     default String getTitle() {
-        return "Tvoje nabídka bude brzy deaktivována.";
+        return "Už jsme o tobě dlouho neslyšeli!";
     }
 
     default String getBody() {
-        return "Už jsme o tobě dlouho neslyšeli! Otevři appku, jinak budou tvoje nabídky kvůli neaktivitě deaktivovány.";
+        return "Máš nabídky? Otevři appku, jinak budou kvůli neaktivitě deaktivovány.";
     }
 
     String getFirebaseToken();
